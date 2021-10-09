58934cb3fce3263f8bb198e15df141f4216d5525
Jobove <OYCuBa@gmail.com>
2021-10-09 17:19:35 +0800

    Add description.

--- src/homework/Lesson5/Lesson5_1.java
+++ src/homework/Lesson5/Lesson5_1.java
@@ -1,8 +1,13 @@
 package homework.Lesson5;
 
-import org.omg.CORBA.ObjectHelper;
-
 public class Lesson5_1 {
+    /**
+     * personAddress类, 存放了姓氏, 名字, 邮箱以及手机号码四个数值成员.
+     * 包含4个Getter和2个Setter.
+     * 提供2个不同的构造函数以对应只提供姓名以及提供姓名、邮箱和手机号码的初始化情景.
+     *
+     * @author 张泽贤
+     */
     public static class personAddress {
         String firstName;
 
@@ -12,35 +17,71 @@ public class Lesson5_1 {
 
         long phoneNumber;
 
+        /**
+         * @return 返回名字.
+         */
         public String getFirstName() {
             return firstName;
         }
 
+        /**
+         * @return 返回姓氏.
+         */
         public String getSecondName() {
             return secondName;
         }
 
+        /**
+         * @return 返回邮箱.
+         */
         public String getEmail() {
             return email;
         }
 
+        /**
+         * @return 返回电话号码.
+         */
         public long getPhoneNumber() {
             return phoneNumber;
         }
 
+        /**
+         * 更改联系人邮箱.
+         * 
+         * @param email 更改的邮箱.
+         */
         public void setEmail(String email) {
             this.email = email;
         }
 
+        /**
+         * 更改联系人手机号码.
+         * 
+         * @param phoneNumber 更改的电话号码.
+         */
         public void setPhoneNumber(long phoneNumber) {
             this.phoneNumber = phoneNumber;
         }
 
+        /**
+         * 构造函数, 仅提供联系人的姓氏与名字.
+         *
+         * @param FirstName  联系人的名字.
+         * @param SecondName 联系人的姓氏.
+         * @see personAddress#personAddress(String, String, String, long) 提供邮箱以及手机号码的构造函数.
+         */
         personAddress(String FirstName, String SecondName) {
             firstName = FirstName;
             secondName = SecondName;
         }
 
+        /**
+         * 提供所有信息的构造函数.
+         *
+         * @param email 联系人邮箱.
+         * @param phoneNumber 联系人手机号码.
+         * @see personAddress#personAddress(String, String) 缺省了邮箱以及手机号码的构造函数.
+         */
         personAddress(String firstName, String secondName, String email, long phoneNumber) {
             this.firstName = firstName;
             this.secondName = secondName;
@@ -48,6 +89,13 @@ public class Lesson5_1 {
             setPhoneNumber(phoneNumber);
         }
 
+        /**
+         * 覆写equals()函数, 仅判断联系人实例姓名是否相等.
+         * 
+         * @param object 一个对象的实例, 可传入任意对象的实例.
+         * @return 返回一个布尔值表示实例是否同为personAddress的实例且为同一联系人(仅比较姓名).
+         * @see Object#equals(Object) 被覆写的父类方法.
+         * */
         @Override
         public boolean equals(Object object) {
             return object.getClass() == personAddress.class
@@ -55,6 +103,12 @@ public class Lesson5_1 {
                     && ((personAddress) object).getSecondName().contentEquals(getSecondName());
         }
 
+        /**
+         * 同时覆写hashCode()方法, 仅考虑了姓氏和名字关键值.
+         *
+         * @return 返回实例的哈希值.
+         * @see Object#hashCode() 被覆写的父类方法.
+         * */
         @Override
         public int hashCode() {
             int result = 17;
--- src/homework/Lesson5/Lesson5_2.java
+++ src/homework/Lesson5/Lesson5_2.java
@@ -1,32 +1,65 @@
 package homework.Lesson5;
 
 public class Lesson5_2 {
+    /**
+     * Beer类, 存放了品牌名和酒精度两个成员变量.
+     * 包含2个Getter和2个Setter.
+     * 提供一个包含两个参数的构造函数.
+     *
+     * @author 张泽贤
+     * */
     public static class Beer {
         String name;
         double alcohol;
 
+        /**
+         * @return 返回酒的名字.
+         * */
         public String getName() {
             return name;
         }
 
+        /**
+         * 更改酒的名字.
+         *
+         * @param name 更改后酒的名字
+         */
         public void setName(String name) {
             this.name = name;
         }
 
+        /**
+         * @return 返回酒的名字.
+         */
         public double getAlcohol() {
             return alcohol;
         }
 
+        /**
+         * 更改酒的酒精度.
+         *
+         * @param alcohol 更改后酒的酒精度.
+         */
         public void setAlcohol(double alcohol) {
             this.alcohol = alcohol;
         }
 
+        /**
+         * 计算一个成年人喝醉所需该种酒的瓶数.
+         *
+         * @param weight 一个成年人的体重, 单位为kg.
+         * @return 返回一个重量为weight的成年人喝醉所需这种酒的瓶数.
+         */
         public double intoxicated(double weight) {
             double numDrinks;
             numDrinks = (0.08 + 0.015) * weight / (12 * 7.5 * alcohol);
             return numDrinks;
         }
 
+        /**
+         * @param name 酒的名字.
+         * @param alcohol 酒的酒精度.
+         */
         Beer(String name, double alcohol) {
             setName(name);
             setAlcohol(alcohol);
